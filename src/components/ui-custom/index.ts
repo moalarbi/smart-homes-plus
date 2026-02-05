15bf72fb6ecc819e363e31dbb120f2984ec2ef51
Mohamed Alarbi <54469631+moalarbi@users.noreply.github.com>
2026-02-05 16:51:04 +0300

    Add index.ts

---
+++ src/components/ui-custom/index.ts
@@ -0,0 +1,6 @@
+export { GlassCard } from './GlassCard';
+export { SEO } from './SEO';
+export { SectionTitle } from './SectionTitle';
+export { StatCard } from './StatCard';
+export { FeatureCard } from './FeatureCard';
+export { StepCard } from './StepCard';
