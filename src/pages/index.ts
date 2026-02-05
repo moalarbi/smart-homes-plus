d964f818f0d8fdcbd661da5195cc4637fdb3d375
Mohamed Alarbi <54469631+moalarbi@users.noreply.github.com>
2026-02-05 16:49:44 +0300

    Add index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { Home } from './Home';
+export { About } from './About';
+export { Products } from './Products';
+export { Solutions } from './Solutions';
+export { Contact } from './Contact';
